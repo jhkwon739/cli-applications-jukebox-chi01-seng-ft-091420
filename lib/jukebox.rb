71df214c40aeaee67c22946d75f979ebb8acac7b
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-22 00:35:41 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -45,7 +45,7 @@ def play(songs)
   if song_number_name_input.to_i == 0
     song_index = songs.index(song_number_name_input) 
     if song_index != nil
-      puts "Playing " + song(song_index)
+      puts "Playing " + songs(song_index)
     else
       puts "Invalid input, please try again"
     end
